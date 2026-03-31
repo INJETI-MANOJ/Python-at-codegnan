1e13aec928d67d2f72b7a6fbb094063056a1ca32
MANOJ <159169606+INJETI-MANOJ@users.noreply.github.com>
2026-03-31 19:50:12 +0530

    Add files via upload

---
+++ Day-2 Python Tokens_variables.py
@@ -0,0 +1,8 @@
+a = 99999
+b = 99999
+c = 30
+d = a+b+c
+print(d)
+print(id(a),id(b),id(c))
+x = 10
+print(type(x))
---
+++ Day-2 Python Work.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+c = a + b
+print('Addition of two numbers', c)
+d = a - b
+print('Substraction of two numbers', d)
+e = a * b
+print('Multiplication of two numbers', e)
+f = a / b
+print('Division of two numbers', f)
+g = a % b
+print('Module of two numbers', g)
+h = a // b
+print('Floor division of two numbers', h)
+i = a ** b
+print('Exponent of two numbers', i)
