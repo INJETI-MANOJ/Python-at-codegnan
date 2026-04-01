cb45edd82710b17c3b0f38405469b57b54af1338
MANOJ <159169606+INJETI-MANOJ@users.noreply.github.com>
2026-04-01 20:12:22 +0530

    Add files via upload

---
+++ Day-3 Python Work.py
@@ -0,0 +1,86 @@
+# Assignment Operators
+a = 10
+
+a += 2
+print(a)
+
+b = 10
+b -= 2
+print(b)
+
+c = 10
+c *= 2
+print(c)
+
+d = 10
+d /= 2
+print(d)
+
+e = 10
+e %= 2
+print(e)
+
+f = 10
+f //= 3
+print(f)
+
+g = 10
+g **= 3
+print(g)
+
+# Relational or comparision Operators
+x = 10
+y = 20
+
+print(x<y)
+print(x>y)
+print(x<=y)
+print(x>=y)
+print(x==y)
+print(x!=y)
+
+
+#Logical Operators
+print(x>y and x>=y)
+print(x>y and x<=y)
+print(x<y and x>=y)
+print(x<y and x<=y)
+
+print(x>y or x>=y)
+print(x>y or x<=y)
+print(x<y or x>=y)
+print(x<y or x<=y)
+
+z = x>y
+print(not z)
+k = x<y
+print(not z)
+
+#Bitwise Operators
+l = 10 << 2
+print(l)
+
+m = 10 >> 2
+print(m)
+
+n = 10 ^ 11
+print(n)
+
+v = 10 & 1
+print(v)
+
+o = 10 | 12
+print(o)
+
+#Membership Operators
+a = [0,1,2,3,4,5]
+print(5 in a)
+print(2 not in a)
+print(6 in a)
+
+#Idemtity Operators
+q = 10
+w = 10
+print(q is w)
+print(q is not w)
+
